1d917b91f42b6a75ed600c7b65ebe8821abcad52
Grewi <grewi@ya.ru>
2023-03-05 18:43:34 +0400

    ++

--- autoloader.php
+++ autoloader.php
@@ -7,8 +7,11 @@ function systemAutoLoader(string $className) {
     //авто переопределение
     $classArr = explode('/', $className);
     $appSystem = ROOT . '/app/' . $className . '.php';
+    $originSystem = ROOT . '/' . str_replace('origin/', '', $className) . '.php';
     if($classArr[0] == 'system' && file_exists($appSystem)){
         includeFile($appSystem);
+    }elseif($classArr[0] == 'system' && file_exists($originSystem)){
+        includeFile(ROOT . '/' . $originSystem . '.php');
     }else{
         includeFile(ROOT . '/' . $className . '.php');
     }
