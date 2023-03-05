b46a50fd21e579223d85f4b4b2f3a5bd40da10df
Grewi <grewi@ya.ru>
2023-03-05 18:50:50 +0400

    app system

--- autoloader.php
+++ autoloader.php
@@ -3,17 +3,6 @@
 
 function systemAutoLoader(string $className) {
     $className = str_replace('\\', '/', $className);
-
-    //авто переопределение
-    $classArr = explode('/', $className);
-    $appSystem = ROOT . '/app/' . $className . '.php';
-    $originSystem = ROOT . '/' . str_replace('origin/', '', $className) . '.php';
-    if($classArr[0] == 'system' && file_exists($appSystem)){
-        includeFile($appSystem);
-    }elseif($classArr[0] == 'system' && file_exists($originSystem)){
-        includeFile(ROOT . '/' . $originSystem . '.php');
-    }else{
-        includeFile(ROOT . '/' . $className . '.php');
-    }
+    includeFile(ROOT . '/' . $className . '.php');
 }
 spl_autoload_register('systemAutoLoader');
