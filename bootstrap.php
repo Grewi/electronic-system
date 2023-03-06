c1aa6541ef038e4467be14948713da68d5c3f8ac
cetmolnia <aifedoseev00@gmail.com>
2023-03-06 11:16:27 +0400

    request_url

--- bootstrap.php
+++ bootstrap.php
@@ -1,7 +1,7 @@
 <?php 
 !INDEX ? exit('exit') : true;
 
-request('global')->set(['uri' => $_SERVER['REQUEST_URI']]);
+request('global')->set(['uri' => isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : '']);
 
 
 if (filter_var(@$_SERVER['HTTP_CLIENT_IP'], FILTER_VALIDATE_IP)) {
