3ef7b4b077d4d7e6baa87e7d8385929440d53c47
Grewi <grewi@ya.ru>
2023-06-29 19:51:33 +0400

    ++

--- console/migrate.php
+++ console/migrate.php
@@ -1,63 +1,78 @@
 <?php
+
 namespace system\console;
+
 use system\core\database\database;
 
 class migrate
 {
-    public function index() : void
+    public function index(): void
     {
         $db = database::connect();
 
         //Автоопределение таблицы миграции в БД
-        try{
+        try {
             $start = $db->fetch('SELECT COUNT(*) as count FROM `migrations`', []);
-        }catch(\PDOException $e){
+        } catch (\PDOException $e) {
             if (!$start) {
-                if(file_exists(APP . '/migrations/0000_00_00.sql')){
-                    $startSql = file_get_contents(APP . '/migrations/0000_00_00.sql');
-                    if(!empty($startSql)){
-                        $db->query($startSql, []);
-                        echo 'Создана стартовая таблица миграции!' . PHP_EOL;
-                    }
-                }
+                $startSql = 'SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+                START TRANSACTION;
+                SET time_zone = "+00:00";
+                
+                CREATE TABLE `migrations` (
+                  `id` int(11) NOT NULL,
+                  `name` varchar(255) NOT NULL,
+                  `active` timestamp NULL DEFAULT NULL
+                ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+                
+                
+                ALTER TABLE `migrations`
+                  ADD PRIMARY KEY (`id`);
+                
+                
+                ALTER TABLE `migrations`
+                  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+                COMMIT;';
+                $db->query($startSql, []);
+                echo 'Создана стартовая таблица миграции!' . PHP_EOL;
             }
         }
-        
+
         $allFiles = scandir(APP . '/migrations');
 
         //Запуск миграции
-            foreach ($allFiles as $i) {
-                if ($i == '.' || $i == '..') {
-                    continue;
-                }
-                $i = str_replace('.sql', '', $i);
-                $m = $db->fetch('SELECT * FROM migrations WHERE name = "' . $i . '"', []);
+        foreach ($allFiles as $i) {
+            if ($i == '.' || $i == '..') {
+                continue;
+            }
+            $i = str_replace('.sql', '', $i);
+            $m = $db->fetch('SELECT * FROM migrations WHERE name = "' . $i . '"', []);
 
-                if (empty($m)) {
-                    
-                    try {
-                        $mSql = file_get_contents(APP . '/migrations/' . $i . '.sql');
-                        if(!empty($mSql)){
-                            $db->query('INSERT INTO migrations SET name = "' . $i . '", active = "' . date('Y-m-d H:i', time()) . '"', []);
-                            $db->query($mSql, []);
-                            echo 'Применён ' . $i  . PHP_EOL;                            
-                        }else{
-                            echo 'Пустой файл миграции ' . $i  . PHP_EOL; 
-                        }
-                    }catch(\PDOException $e){
-                        echo $e->getMessage()   . PHP_EOL;
+            if (empty($m)) {
+
+                try {
+                    $mSql = file_get_contents(APP . '/migrations/' . $i . '.sql');
+                    if (!empty($mSql)) {
+                        $db->query('INSERT INTO migrations SET name = "' . $i . '", active = "' . date('Y-m-d H:i', time()) . '"', []);
+                        $db->query($mSql, []);
+                        echo 'Применён ' . $i  . PHP_EOL;
+                    } else {
+                        echo 'Пустой файл миграции ' . $i  . PHP_EOL;
                     }
-                }else{
-                    echo 'Пропущен ' . $i  . PHP_EOL; 
+                } catch (\PDOException $e) {
+                    echo $e->getMessage()   . PHP_EOL;
                 }
-            }  
+            } else {
+                echo 'Пропущен ' . $i  . PHP_EOL;
+            }
+        }
     }
 
-    public function createMigration() : void
+    public function createMigration(): void
     {
         $parametr = ARGV[2];
-        $s = preg_replace( "/[^a-zA-Z0-9\s]/", '_', $parametr );
-        $fileName = APP . '/migrations/' . date('Y_m_d_U') . '_' . $s . '.sql'; 
+        $s = preg_replace("/[^a-zA-Z0-9\s]/", '_', $parametr);
+        $fileName = APP . '/migrations/' . date('Y_m_d_U') . '_' . $s . '.sql';
         file_put_contents($fileName, '');
     }
 }
