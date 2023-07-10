3c7bc03ee7be8dc4f0338b3a823f944cf5ce0c96
cetmolnia <aifedoseev00@gmail.com>
2023-07-10 16:41:03 +0400

    ++

--- core/model/traits/delete.php
+++ core/model/traits/delete.php
@@ -5,13 +5,14 @@ trait delete
 {
     private function delete($id = null): void
     {
-        if($this->_id){
-            $this->where($this->_id);
-        }
+
         if(is_null($id)){
             $sql = 'DELETE FROM ' . $this->_table . ' ' . $this->_where;
             db()->query($sql, $this->_bind);
         }else{
+            if($this->_id){
+                $this->where($this->_id);
+            }            
             $sql = 'DELETE FROM ' . $this->_table . ' WHERE `' . $this->_id . '` = ' . $id;
             db()->query($sql, [$this->_id => $id]);
         }
