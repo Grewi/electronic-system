027230d147be6cbfe40e25e06458c40ad38bf6ab
Grewi <grewi@ya.ru>
2023-07-30 01:24:45 +0400

    fix model delete

--- core/model/traits/delete.php
+++ core/model/traits/delete.php
@@ -6,15 +6,15 @@ trait delete
     private function delete($id = null): void
     {
 
-        if(is_null($id)){
+        if(is_null($id) && is_null($this->{$this->_id})){
             $sql = 'DELETE FROM ' . $this->_table . ' ' . $this->_where;
             db()->query($sql, $this->_bind);
         }else{
-            if($this->_id){
-                $this->where($this->_id);
+            if($this->{$this->_id}){
+                $this->where($this->{$this->_id});
             }            
-            $sql = 'DELETE FROM ' . $this->_table . ' WHERE `' . $this->_id . '` = ' . $id;
-            db()->query($sql, [$this->_id => $id]);
+            $sql = 'DELETE FROM ' . $this->_table . ' ' .  $this->_where;
+            db()->query($sql, $this->_bind);
         }
 
     }
