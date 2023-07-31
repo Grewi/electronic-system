45d3c4914fa4bb76ed4cdf14770c87ccb8cffa5e
Grewi <grewi@ya.ru>
2023-07-31 22:58:22 +0400

    ++

--- function/route.php
+++ function/route.php
@@ -4,6 +4,7 @@ function autoloadWeb($route)
 {
     $existInclude = get_included_files();
     $appFunctionDir = APP . '/route/web';
+    $generatorPage = null;
     if (file_exists($appFunctionDir)) {
         $systemFnctionFiles = scandir($appFunctionDir);
         if (is_iterable($systemFnctionFiles)) {
@@ -14,11 +15,19 @@ function autoloadWeb($route)
                 if (in_array($appFunctionDir . '/' . $file, $existInclude)) {
                     continue;
                 }
+                if($file == 'generatorPage.php'){
+                    $generatorPage = $appFunctionDir . '/' . $file;
+                    continue;
+                }
+                
                 $f = pathinfo($file);
                 if ($f['extension'] == 'php') {
                     require $appFunctionDir . '/' . $file;
                 }
             }
+        }
+        if($generatorPage){
+            require $generatorPage;
         }
     }
 }
