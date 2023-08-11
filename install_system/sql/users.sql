6968b705c73d80810e3c33300178b67d86a6001e
grewi <grewi@ya.ru>
2023-08-11 08:54:21 +0400

    fix

--- install_system/sql/users.sql
+++ install_system/sql/users.sql
@@ -17,7 +17,7 @@ CREATE TABLE `users` (
 
 
 INSERT INTO `users` (`id`, `email`, `email_code`, `email_status`, `password`, `name`, `login`, `active`, `user_role_id`, `date_create`) VALUES
-(1, 'test@test.ru', 0000, 1, '$2y$10$CgcnD9KQqVjnmUH87/rqBuemv94y8lfzGgLXU3UvoeznI9TL9q3Gm', 'Евгений', NULL, 1, 1, '2022-10-21 05:54:22');
+(1, 'test@test.ru', 0000, 1, '$2y$10$CgcnD9KQqVjnmUH87/rqBuemv94y8lfzGgLXU3UvoeznI9TL9q3Gm', 'admin', 'admin', 1, 1, '2022-10-21 05:54:22');
 
 
 ALTER TABLE `users`
