8789bbfaa090ff6fc5f3ece714bf1be2c6fc2080
Grewi <grewi@ya.ru>
2024-10-29 10:21:01 +0400

    default sort

--- core/model/traits/sort.php
+++ core/model/traits/sort.php
@@ -6,11 +6,16 @@ trait sort
     /**
      * Обработка get параметров sort 
      * @param array $listCol - Список колонок по которым допускается сортировка
+     * @param string $defaultSort - стролбец по умолчанию
+     * @param string $defaultDirection - направление по умолчанию
      * @return mixed
      */
-    protected function sorting(array $listCol = [])
+    protected function sorting(array $listCol = [], $defaultSort = '', $defaultDirection = '')
     {
         if(!isset($_GET['sort']) || !in_array($_GET['sort'], $listCol)){
+            if(!empty($defaultSort) && !empty($defaultDirection)){
+                $this->sort($defaultDirection, $defaultSort);
+            }
             return $this;
         }
         if(isset($_GET['direction']) && $_GET['direction'] == 'desc'){
